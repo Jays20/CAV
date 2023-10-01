5dc338eca463cba2020eae392bc95a3edabd4e76
Minh Nguyen <minh.nguyen6@monash.edu>
2023-10-01 14:41:01 +1100

    Add results.

--- simulation/environment.py
+++ simulation/environment.py
@@ -384,12 +384,14 @@ class CarlaEnvironment():
         try:
             # spawn_points_town06 = [279, 12, 275, 280, 13, 276]
             spawn_points_town06 = [12, 275]
+            blueprints_to_exclude = ["vehicle.diamondback.century", "vehicle.carlamotors.carlacola"]
             for _ in range(0, len(spawn_points_town06)):
                 spawn_point = self.map.get_spawn_points()[spawn_points_town06[_]]
-                bp_vehicle = random.choice(self.blueprint_library.filter('vehicle'))
+                vehicle_blueprints = [bp for bp in self.blueprint_library.filter('vehicle') if bp.id not in blueprints_to_exclude]
+                bp_vehicle = random.choice(vehicle_blueprints)
                 other_vehicle = self.world.try_spawn_actor(bp_vehicle, spawn_point)
                 if other_vehicle is not None:
-                    other_vehicle.apply_control(carla.VehicleControl(throttle=0.5))
+                    other_vehicle.apply_control(carla.VehicleControl(throttle=1))
                     self.actor_list.append(other_vehicle)
         except:
             print('Exception has occured in spawning vehicles')
